ee7b1a17470cbdf057fca91c5c38a43f1023c08a
wangchuanchuan <wangchuanchuan@360.cn>
2017-06-01 18:00:41 +0800

    deploy忽略database配置

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 type=$1
+excludeCmd="--exclude=application/config/database.php"
 if [ $type -eq "1" ]
 then
-    rsync -tpcrv --delay-updates --timeout=60 ./* filmfilm@166.62.86.13:/home/www/film/
+    rsync -tpcrv --delay-updates --timeout=60 $excludeCmd ./* filmfilm@166.62.86.13:/home/www/film/
 else
-    rsync -tpcrv --delay-updates --timeout=60 ./* root@120.76.76.195:/home/wangchuan/film/
+    rsync -tpcrv --delay-updates --timeout=60 $excludeCmd ./* root@120.76.76.195:/home/wangchuan/film/
 fi
