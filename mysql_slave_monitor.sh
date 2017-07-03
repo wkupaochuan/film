77f68fa79ddc00b4feee728bc68a9e9fa3600868
wkupaochuan <wkupaochuan@126.com>
2017-07-03 18:09:44 +0800

    mysql 监控同时检测互为主从的机器

--- mysql_slave_monitor.sh
+++ mysql_slave_monitor.sh
@@ -3,7 +3,20 @@ slave_is=($(ssh filmfilm@166.62.86.13 "mysql -uroot -proot -e 'use skin;show sla
 
 if [ "${slave_is[0]}" == "Yes" -a "${slave_is[1]}" == "Yes" ]
 then
-        echo `date "+%Y-%m-%d %H:%M:%S"` "ok"
+        echo film `date "+%Y-%m-%d %H:%M:%S"` "ok"
 else
-        echo `date "+%Y-%m-%d %H:%M:%S"` "no"
+        echo film `date "+%Y-%m-%d %H:%M:%S"` "no"
 fi
+
+ali_slave_is=($(mysql -S /data/mysql/3307/mysql.sock -uroot -proot -e "use skin;show slave status\G;" | grep  Slave_.*_Running | awk '{print $2}'))
+
+if [ "${ali_slave_is[0]}" == "Yes" -a  "${ali_slave_is[1]}" == "Yes" ]
+then
+        echo ali `date "+%Y-%m-%d %H:%M:%S"` "ok"
+else
+        echo ali `date "+%Y-%m-%d %H:%M:%S"` "no"
+fi
+
+echo -e "\n"
+
+exit
